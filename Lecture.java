f692a9d7a07657a5f193641cd20e4cbe76f4c059
Feiyu Chen <chenfeiyu@chenfeiyudeMacBook-Pro.local>
2018-11-06 15:16:11 +0800

    push lecture.java

---
+++ Lecture.java
@@ -0,0 +1,12 @@
+public class Lecture implements Session{
+	private int vacancy;
+	private int size;
+	private int id;
+
+	public int getVacancy(){
+		return vacancy;
+	}
+	public int getSize(){
+		return getSize;
+	}
+}
