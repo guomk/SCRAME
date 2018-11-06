e7a7beac745f5825a4ea8a55017309e478638146
Feiyu Chen <chenfeiyu@chenfeiyudeMacBook-Pro.local>
2018-11-06 15:22:03 +0800

    edit tutorial.java

---
+++ Tutorial.java
@@ -0,0 +1,12 @@
+public class Tutorial(){
+	private int size;
+	private int id;
+	private int vacancy;
+
+	public int getVacancy(){
+		return vacncy;
+	}
+	public int getSize(){
+		return size;
+	}
+}
