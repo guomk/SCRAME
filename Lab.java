f28c85313dad4ded0f71e5569d860fe5845c36ce
Zhiwei Guo <goozwjenny@gmail.com>
2018-11-06 15:16:28 +0800

    Added Lab.java

---
+++ Lab.java
@@ -0,0 +1,14 @@
+public class Lab{
+	
+	private int vacancy;
+	private int size;
+	private int id;
+
+	public int getVcancy(){
+		return vacancy;
+	}
+
+	public int getSize(){
+		return size;
+	}
+}
